b5fdf57b0913b0859dd1916bfb238ba7f1651d2a
hanzhang418 <hanzhang2566@gmail.com>
2025-04-16 11:32:38 +0800

    feat(config): 添加配置管理

---
+++ config/config.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	_ "embed"
+	"fmt"
+	"github.com/spf13/viper"
+	"layout-for-go/pkg/env"
+	"path/filepath"
+	"sync"
+)
+
+// Config 映射 config/*.yaml 中配置的结构体
+type Config struct {
+	Profile string `mapstructure:"profile"`
+	Default struct {
+		Name string `mapstructure:"name"`
+		Age  int    `mapstructure:"age"`
+	} `mapstructure:"default"`
+}
+
+// Init 初始化项目配置
+func Init() Config {
+	viper.SetConfigFile(filepath.Join("config", fmt.Sprintf("%s.yaml", env.Current())))
+
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
+	}
+
+	var cfg Config
+	if err := viper.Unmarshal(&cfg); err != nil {
+		panic(err)
+	}
+
+	return cfg
+}
+
+// 初始化全局 Config 变量
+var (
+	once     sync.Once
+	instance Config
+)
+
+// Get 单例当前项目配置
+func Get() Config {
+	once.Do(func() {
+		instance = Init()
+	})
+
+	return instance
+}
